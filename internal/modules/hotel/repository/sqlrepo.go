48955202253993582cffecf450f28bdac20c5072
afif0808 <afif.panai@gmail.com>
2021-12-30 15:39:36 +0700

    Implement create , update and get-list of hotel

--- internal/modules/hotel/repository/sqlrepo.go
+++ internal/modules/hotel/repository/sqlrepo.go
@@ -1,1 +1,51 @@
 package repository
+
+import (
+	"context"
+
+	"github.com/afif0808/bobobox_test/models"
+	"github.com/afif0808/bobobox_test/pkg/sqls"
+
+	"github.com/jmoiron/sqlx"
+)
+
+const (
+	tableName = "hotels"
+)
+
+type SQLRepo struct {
+	readDB, writeDB *sqlx.DB
+}
+
+func NewHotelSQLRepo(readDB, writeDB *sqlx.DB) *SQLRepo {
+	repo := SQLRepo{readDB: readDB, writeDB: writeDB}
+	return &repo
+}
+
+func (repo *SQLRepo) InsertHotel(ctx context.Context, h *models.Hotel) error {
+	query, args := sqls.GenerateInsertQuery(tableName, *h)
+	_, err := repo.writeDB.ExecContext(ctx, query, args...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (repo *SQLRepo) UpdateHotel(ctx context.Context, h models.Hotel, id int64) error {
+	query, args := sqls.GenerateUpdateByIDQuery(tableName, h, id)
+	_, err := repo.writeDB.ExecContext(ctx, query, args...)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (repo *SQLRepo) FetchHotels(ctx context.Context) ([]models.Hotel, error) {
+	query := "SELECT * FROM " + tableName
+	var hs []models.Hotel
+	err := repo.readDB.SelectContext(ctx, &hs, query)
+	if err != nil {
+		return nil, err
+	}
+	return hs, nil
+
+}
