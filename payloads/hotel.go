ab146d86d637fa214c4807e81c53add04c0e8d7e
afif0808 <afif.panai@gmail.com>
2021-12-30 15:41:12 +0700

    Define hotel payloads

--- payloads/hotel.go
+++ payloads/hotel.go
@@ -1,1 +1,17 @@
 package payloads
+
+type CreateHotelPayload struct {
+	Name    string `json:"name" merge:"name"`
+	Address string `json:"address" merge:"address"`
+}
+
+type UpdateHotelPayload struct {
+	Name    string `json:"name" merge:"name"`
+	Address string `json:"address" merge:"address"`
+}
+
+type HotelPayload struct {
+	ID      int64  `json:"id" merge:"id"`
+	Name    string `json:"name" merge:"name"`
+	Address string `json:"address" merge:"address"`
+}
