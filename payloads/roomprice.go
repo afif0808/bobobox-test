beaa29d2b42d53317f4f9357110d7bbf47309ee2
afif0808 <afif.panai@gmail.com>
2021-12-31 20:22:34 +0700

    Define room price payload

---
+++ payloads/roomprice.go
@@ -0,0 +1,18 @@
+package payloads
+
+type CreateRoomPricePayload struct {
+	Date       string  `json:"date" merge:"date"`
+	UntilDate  string  `json:"until_date"`
+	Price      float64 `json:"price" merge:"price"`
+	RoomTypeID int64   `json:"room_type_id" merge:"room_type_id"`
+}
+type UpdateRoomPricePayload struct {
+	Price float64 `json:"price" merge:"price"`
+}
+
+type RoomPricePayload struct {
+	ID         int64   `json:"id"  merge:"id"`
+	Date       string  `json:"date"  merge:"date"`
+	Price      float64 `json:"price"  merge:"price"`
+	RoomTypeID int64   `json:"room_type_id"  merge:"room_type_id"`
+}
