3a27d0e764b6c7f2268d38fc4ea529ee46bdeee0
afif0808 <afif.panai@gmail.com>
2021-12-31 13:38:23 +0700

    Refactor code, shorten codes by using another package and fix not found error for update and delete

--- internal/modules/hotel/repository/sqlrepo.go
+++ internal/modules/hotel/repository/sqlrepo.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"context"
 	"database/sql"
-	"errors"
 
 	"github.com/afif0808/bobobox_test/models"
 	"github.com/afif0808/bobobox_test/pkg/customerrors"
@@ -26,28 +25,18 @@ func NewHotelSQLRepo(readDB, writeDB *sqlx.DB) *HotelSQLRepo {
 }
 
 func (repo *HotelSQLRepo) InsertHotel(ctx context.Context, h *models.Hotel) error {
-	query, args := sqls.GenerateInsertQuery(tableName, *h)
-	_, err := repo.writeDB.ExecContext(ctx, query, args...)
+	err := sqls.Insert(ctx, repo.writeDB, tableName, h)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 func (repo *HotelSQLRepo) UpdateHotel(ctx context.Context, h models.Hotel, id int64) error {
-	query, args := sqls.GenerateUpdateByIDQuery(tableName, h, id)
-	res, err := repo.writeDB.ExecContext(ctx, query, args...)
-	if err != nil {
-		return err
+	err := sqls.Update(ctx, repo.writeDB, tableName, h, id)
+	if err == sql.ErrNoRows {
+		return customerrors.NewCustomError("hotel is not found", err, customerrors.ErrTypeNotFound)
 	}
-	affected, err := res.RowsAffected()
-	if err != nil {
-		return err
-	}
-	if affected < 1 {
-		return customerrors.NewCustomError("hotel is not found", errors.New("no rows is affected"), customerrors.ErrTypeNotFound)
-	}
-
-	return nil
+	return err
 }
 
 func (repo *HotelSQLRepo) FetchHotels(ctx context.Context) ([]models.Hotel, error) {
@@ -61,30 +50,19 @@ func (repo *HotelSQLRepo) FetchHotels(ctx context.Context) ([]models.Hotel, erro
 }
 
 func (repo *HotelSQLRepo) DeleteHotel(ctx context.Context, id int64) error {
-	query := "DELETE FROM " + tableName + " WHERE id = ?"
-	res, err := repo.writeDB.ExecContext(ctx, query, id)
-	if err != nil {
-		return err
+	err := sqls.Delete(ctx, repo.writeDB, tableName, id)
+	if err == sql.ErrNoRows {
+		return customerrors.NewCustomError("hotel is not found", err, customerrors.ErrTypeNotFound)
 	}
-	affected, err := res.RowsAffected()
-	if err != nil {
-		return err
-	}
-	if affected < 1 {
-		return customerrors.NewCustomError("hotel is not found", errors.New("no rows is affected"), customerrors.ErrTypeNotFound)
-	}
-
-	return nil
+	return err
 }
 
 func (repo *HotelSQLRepo) GetHotel(ctx context.Context, id int64) (models.Hotel, error) {
 	query := "SELECT * FROM " + tableName + " WHERE id = ?"
 	var h models.Hotel
 	err := repo.readDB.GetContext(ctx, &h, query, id)
-	if err != nil && err == sql.ErrNoRows {
+	if err == sql.ErrNoRows {
 		return h, customerrors.NewCustomError("hotel is not found", err, customerrors.ErrTypeNotFound)
-	} else if err != nil {
-		return h, err
 	}
-	return h, nil
+	return h, err
 }
