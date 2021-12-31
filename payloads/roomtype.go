2428a67ebc8dd3c531a0846285d065801cf750b6
afif0808 <afif.panai@gmail.com>
2021-12-31 20:22:14 +0700

    Add default price field

--- payloads/roomtype.go
+++ payloads/roomtype.go
@@ -1,13 +1,17 @@
 package payloads
 
 type CreateRoomTypePayload struct {
-	Name string `json:"name" merge:"name"`
+	Name         string  `json:"name" merge:"name"`
+	DefaultPrice float64 `json:"default_price" merge:"default_price"`
 }
+
 type UpdateRoomTypePayload struct {
-	Name string `json:"name" merge:"name"`
+	Name         string  `json:"name" merge:"name"`
+	DefaultPrice float64 `json:"default_price" merge:"default_price"`
 }
 
 type RoomTypePayload struct {
-	ID   int64  `json:"id" merge:"id"`
-	Name string `json:"name" merge:"name"`
+	ID           int64   `json:"id" merge:"id"`
+	Name         string  `json:"name" merge:"name"`
+	DefaultPrice float64 `json:"default_price" merge:"default_price"`
 }
