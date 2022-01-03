36526bad76130a27af246a48b5b5ebc19c508815
afif0808 <afif.panai@gmail.com>
2022-01-03 18:56:19 +0700

    Migrate database and inject modules

--- main.go
+++ main.go
@@ -5,10 +5,21 @@ import (
 	"log"
 	"os"
 
+	"github.com/afif0808/bobobox_test/internal/modules/hotel"
+	"github.com/afif0808/bobobox_test/internal/modules/promo"
+	"github.com/afif0808/bobobox_test/internal/modules/reservation"
+	roomprice "github.com/afif0808/bobobox_test/internal/modules/room/price"
+	"github.com/afif0808/bobobox_test/internal/modules/room/room"
+	roomtype "github.com/afif0808/bobobox_test/internal/modules/room/typ"
+	"github.com/afif0808/bobobox_test/internal/modules/stay"
+
+	"github.com/afif0808/bobobox_test/models"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -18,9 +29,19 @@ func main() {
 	}
 	e := echo.New()
 
+	readDB, writeDB := initMySQLDB()
+	migrateDatabase()
+
+	hotel.InjectHotelModule(e, readDB, writeDB)
+	roomtype.InjectRoomTypeModule(e, readDB, writeDB)
+	roomprice.InjectRoomPriceModule(e, readDB, writeDB)
+	room.InjectRoomModule(e, readDB, writeDB)
+	reservation.InjectReservationModule(e, readDB, writeDB)
+	promo.InjectPromoModule(e, readDB, writeDB)
+	stay.InjectStayModule(e, readDB, writeDB)
+
 	e.Start(":8080")
 }
-
 func initMySQLDB() (readDB, writeDB *sqlx.DB) {
 	host := os.Getenv("MYSQL_HOST")
 	user := os.Getenv("MYSQL_USER")
@@ -28,8 +49,7 @@ func initMySQLDB() (readDB, writeDB *sqlx.DB) {
 	db := os.Getenv("MYSQL_DATABASE")
 	port := os.Getenv("MYSQL_PORT")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, password, host, port, db)
-	log.Println(dsn)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&multiStatements=true", user, password, host, port, db)
 	readDB, err := sqlx.Open("mysql", dsn)
 	if err != nil {
 		log.Panic(err)
@@ -49,4 +69,29 @@ func initMySQLDB() (readDB, writeDB *sqlx.DB) {
 	}
 
 	return
+}
+
+func migrateDatabase() {
+	host := os.Getenv("MYSQL_HOST")
+	user := os.Getenv("MYSQL_USER")
+	password := os.Getenv("MYSQL_PASSWORD")
+	db := os.Getenv("MYSQL_DATABASE")
+	port := os.Getenv("MYSQL_PORT")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&multiStatements=true", user, password, host, port, db)
+	conn, err := gorm.Open(mysql.Open(dsn))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = conn.AutoMigrate(
+		&models.Hotel{},
+		&models.RoomType{}, &models.RoomPrice{}, &models.Room{},
+		&models.Reservation{}, &models.Stay{}, &models.StayDate{},
+		&models.Promo{}, &models.PromoUse{},
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
 }
