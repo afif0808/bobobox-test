1cfcd9be4883707e26118a615d1dc9875dbc2fa4
afif0808 <afif.panai@gmail.com>
2022-01-03 22:17:36 +0700

    Fix misplaced tag

--- payloads/promo.go
+++ payloads/promo.go
@@ -6,39 +6,39 @@ import (
 
 type PromoPayload struct {
 	ID            int64          `json:"id" merge:"id"`
+	Type          string         `json:"type" merge:"type" ` // amount or percentage
+	Value         float64        `json:"value" merge:"value" `
+	CheckInDays   []time.Weekday `json:"check_in_days" `
+	BookingDays   []time.Weekday `json:"booking_days" `
+	BookingHour   string         `json:"booking_hour" merge:"booking_hour" ` // eg: 08.00-23.00
+	Quota         int            `json:"quota" merge:"quota"`
+	DailyMaxQuota int            `json:"daily_quota" merge:"daily_max_quota" `
+	MinimumNight  int            `json:"minimum_night" merge:"minimum_night"`
+	MinimumRoom   int            `json:"minimum_room" merge:"minimum_room"`
+}
+
+type CreatePromoPayload struct {
 	Type          string         `json:"type" merge:"type" valid:"required"` // amount or percentage
 	Value         float64        `json:"value" merge:"value" valid:"required"`
-	CheckInDays   []time.Weekday `json:"check_in_days" valid:"required"`
-	BookingDays   []time.Weekday `json:"booking_days" valid:"required"`
-	BookingHour   string         `json:"booking_hour" merge:"booking_hour" valid:"required"` // eg: 08.00-23.00
+	CheckInDays   []time.Weekday `json:"check_in_days"  valid:"required"`
+	BookingDays   []time.Weekday `json:"booking_days" valid:"required" `
+	BookingHour   string         `json:"booking_hour" merge:"booking_hour" valid:"required"`
 	Quota         int            `json:"quota" merge:"quota" valid:"required"`
-	DailyMaxQuota int            `json:"daily_quota" merge:"daily_max_quota" valid:"required"`
+	DailyMaxQuota int            `json:"daily_max_quota" merge:"daily_max_quota" valid:"required"`
 	MinimumNight  int            `json:"minimum_night" merge:"minimum_night" valid:"required"`
 	MinimumRoom   int            `json:"minimum_room" merge:"minimum_room" valid:"required"`
 }
 
-type CreatePromoPayload struct {
-	Type          string         `json:"type" merge:"type"` // amount or percentage
-	Value         float64        `json:"value" merge:"value"`
-	CheckInDays   []time.Weekday `json:"check_in_days" `
-	BookingDays   []time.Weekday `json:"booking_days" `
-	BookingHour   string         `json:"booking_hour" merge:"booking_hour"`
-	Quota         int            `json:"quota" merge:"quota"`
-	DailyMaxQuota int            `json:"daily_max_quota" merge:"daily_max_quota"`
-	MinimumNight  int            `json:"minimum_night" merge:"minimum_night"`
-	MinimumRoom   int            `json:"minimum_room" merge:"minimum_room"`
-}
-
 type UpdatePromoPayload struct {
-	Type          string         `json:"type" merge:"type"` // amount or percentage
-	Value         float64        `json:"value" merge:"value"`
-	CheckInDays   []time.Weekday `json:"check_in_days" `
-	BookingDays   []time.Weekday `json:"booking_days" `
-	BookingHour   string         `json:"booking_hour" merge:"booking_hour"`
-	Quota         int            `json:"quota" merge:"quota"`
-	DailyMaxQuota int            `json:"daily_max_quota" merge:"daily_max_quota"`
-	MinimumNight  int            `json:"minimum_night" merge:"minimum_night"`
-	MinimumRoom   int            `json:"minimum_room" merge:"minimum_room"`
+	Type          string         `json:"type" merge:"type" valid:"required"` // amount or percentage
+	Value         float64        `json:"value" merge:"value" valid:"required"`
+	CheckInDays   []time.Weekday `json:"check_in_days"  valid:"required"`
+	BookingDays   []time.Weekday `json:"booking_days"  valid:"required"`
+	BookingHour   string         `json:"booking_hour" merge:"booking_hour" valid:"required"`
+	Quota         int            `json:"quota" merge:"quota" valid:"required"`
+	DailyMaxQuota int            `json:"daily_max_quota" merge:"daily_max_quota" valid:"required"`
+	MinimumNight  int            `json:"minimum_night" merge:"minimum_night" valid:"required"`
+	MinimumRoom   int            `json:"minimum_room" merge:"minimum_room" valid:"required"`
 }
 
 type CheckPromoPayload struct {
