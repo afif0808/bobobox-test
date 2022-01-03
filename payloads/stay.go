96c822358d59a5139feabbecbf677758870922ce
afif0808 <afif.panai@gmail.com>
2022-01-03 18:53:32 +0700

    Redefine some payload

--- payloads/stay.go
+++ payloads/stay.go
@@ -6,5 +6,14 @@ type CreateStayPayload struct {
 }
 
 type StayPayload struct {
-	ID int64 `json:"id"`
+	ID            int64              `json:"id" merge:"id" `
+	RoomID        int64              `json:"room_id" merge:"room_id"`
+	GuestName     string             `json:"guest_name" merge:"guest_name"`
+	ReservationID int64              `json:"reservation_id" merge:"reservation_id"`
+	Reservation   ReservationPayload `json:"reservation" `
+	Dates         []StayDatePayload  `json:"dates"`
+}
+
+type StayDatePayload struct {
+	Date string `json:"date"`
 }
