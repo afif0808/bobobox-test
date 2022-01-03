0408c5577c360ebfb3f8575481c56c0ef04c0072
afif0808 <afif.panai@gmail.com>
2022-01-03 13:31:45 +0700

    Define promo payloads

---
+++ payloads/promo.go
@@ -0,0 +1,63 @@
+package payloads
+
+import (
+	"time"
+)
+
+type PromoPayload struct {
+	ID            int64          `json:"id" merge:"id"`
+	Type          string         `json:"type" merge:"type"` // amount or percentage
+	Value         float64        `json:"value" merge:"value"`
+	CheckInDays   []time.Weekday `json:"check_in_days"`
+	BookingDays   []time.Weekday `json:"booking_days"`
+	BookingHour   string         `json:"booking_hour" merge:"booking_hour"`
+	Quota         int            `json:"quota" merge:"quota"`
+	DailyMaxQuota int            `json:"daily_quota" merge:"daily_max_quota"`
+	MinimumNight  int            `json:"minimum_night" merge:"minimum_night"`
+	MinimumRoom   int            `json:"minimum_room" merge:"minimum_room"`
+}
+
+type CreatePromoPayload struct {
+	Type          string         `json:"type" merge:"type"` // amount or percentage
+	Value         float64        `json:"value" merge:"value"`
+	CheckInDays   []time.Weekday `json:"check_in_days" `
+	BookingDays   []time.Weekday `json:"booking_days" `
+	BookingHour   string         `json:"booking_hour" merge:"booking_hour"`
+	Quota         int            `json:"quota" merge:"quota"`
+	DailyMaxQuota int            `json:"daily_max_quota" merge:"daily_max_quota"`
+	MinimumNight  int            `json:"minimum_night" merge:"minimum_night"`
+	MinimumRoom   int            `json:"minimum_room" merge:"minimum_room"`
+}
+
+type UpdatePromoPayload struct {
+	Type          string         `json:"type" merge:"type"` // amount or percentage
+	Value         float64        `json:"value" merge:"value"`
+	CheckInDays   []time.Weekday `json:"check_in_days" `
+	BookingDays   []time.Weekday `json:"booking_days" `
+	BookingHour   string         `json:"booking_hour" merge:"booking_hour"`
+	Quota         int            `json:"quota" merge:"quota"`
+	DailyMaxQuota int            `json:"daily_max_quota" merge:"daily_max_quota"`
+	MinimumNight  int            `json:"minimum_night" merge:"minimum_night"`
+	MinimumRoom   int            `json:"minimum_room" merge:"minimum_room"`
+}
+
+type CheckPromoPayload struct {
+	PromoID int64            `json:"promo_id"`
+	Rooms   []CheckPromoRoom `json:"rooms"`
+	// TotalPrice float64 `json:"total_price"`
+	// Decided not to use total price field in order to
+	// apply single source of truth principle, so total price will be calculated in the backend
+}
+
+type CheckPromoRoom struct {
+	CheckInDate  string  `json:"check_in_date"`
+	CheckOutDate string  `json:"check_out_date"`
+	Price        float64 `json:"price"`
+}
+
+type CheckPromoResultPayload struct {
+	DefaultTotalPrice float64          `json:"default_total_price"`
+	FinalPrice        float64          `json:"final_price"`
+	TotalPromo        float64          `json:"total_promo"`
+	Rooms             []CheckPromoRoom `json:"rooms"`
+}
